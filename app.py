790efcdb0a034847534650fea3398b551b3c0e4b
Pedro Mantovani <mantovani.pedro99@gmail.com>
2022-10-03 20:42:06 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template, json, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/api/upper', methods=['POST'])
+def upper():
+
+    json = request.get_json()
+    first_name = json['first'].upper()
+    last_name = json['last'].upper()
+    valor = json['combo'].upper()
+    print(f'{first_name} - {last_name} - {valor}')
+    return jsonify(first_name=first_name,last_name=last_name,valor=valor)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
